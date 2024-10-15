a7158aeb9d9fec0bebeb0ef25afe236feb8328de
kim <kimbundi85@gmail.com>
2024-10-15 16:30:08 +0300

    first commit

--- Backend/controllers/Usercontroller.js
+++ Backend/controllers/Usercontroller.js
@@ -1,6 +1,6 @@
 import userModel from "../models/Usermodel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import validator from "validator"
 import { response } from "express";
 
