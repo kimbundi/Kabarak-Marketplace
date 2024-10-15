6743b577d3321b329e95ed2b0be00f70238faed7
kim <kimbundi85@gmail.com>
2024-10-15 16:47:08 +0300

    first commit

--- Frontend/src/pages/Placeorder/Placeorder.jsx
+++ Frontend/src/pages/Placeorder/Placeorder.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import './Placeorder.css'
+import './placeorder.css'
 import { Storecontext } from '../../context/Storecontext'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
